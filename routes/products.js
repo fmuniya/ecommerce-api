20b9e65013eea4bc7ab61a6ebc346362252f3c4d
Freeman Muniya <fmuniya@gmail.com>
2025-07-12 12:00:55 +0100

    updated products with new routes

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getAllProducts } = require('../controllers/productsController');
+const {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct
+} = require('../controllers/productsController');
 
 router.get('/', getAllProducts);
+router.get('/:id', getProductById);
+router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+
 
 module.exports = router;
