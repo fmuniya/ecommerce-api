19c72f6ff4b3e78ed4f6cb6646e4593ee4254c90
Freeman Muniya <fmuniya@gmail.com>
2025-07-14 14:24:49 +0100

    added admin access to delete and update order statuses

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express();
 const usersRouter = require('./routes/users');
 const productsRouter = require('./routes/products');
 const cartRouter = require('./routes/cart');
+const ordersRouter = require('./routes/orders');
 
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/cart', cartRouter);
+app.use('/api/orders', ordersRouter);
 
 app.get('/', (req, res) => {
   res.send('E-Commerce API running');
--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { getUserOrders, getOrderById } = require('../controllers/ordersController');
+const { 
+    getUserOrders, 
+    getOrderById, 
+    getAllOrders,
+    deleteOrder,
+    updateOrderStatus
+} = require('../controllers/ordersController');
 const { authenticateToken } = require('../middleware/auth');
 
+//USER or ADMIN
 router.get('/', authenticateToken, getUserOrders);
 router.get('/:orderId', authenticateToken, getOrderById);
+
+//ADMIN ONLY
+router.delete('/:orderId', authenticateToken, authorizeRole('admin'), deleteOrder);
+router.put('/:orderId', authenticateToken, authorizeRole('admin'), updateOrderStatus);
 
 module.exports = router;
